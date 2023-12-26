09f6dbad45c5e26ab62ea4b82c2c1724ed2f94f9
misung.dev <misung.dev@gmail.com>
2023-12-27 06:21:19 +0900

    Modify: 로그인 페이지 CSS 설정

--- src/Pages/login.js
+++ src/Pages/login.js
@@ -1,25 +1,73 @@
-import React, { useState } from 'react';
-import Button from '../Components/button'
+import React, { useState } from "react";
+import Button from "../Components/button";
 import { useNavigate } from "react-router-dom";
 
-const Login = () =>{
-  const movePage = useNavigate();
-  const [ID, setID] = useState();
+const Login = () => {
+	const movePage = useNavigate();
+	const [ID, setID] = useState();
 	const [PW, setPW] = useState();
 
-  function gomain(){
-    movePage('/main');
-  }
+	function gomain() {
+		movePage("/main");
+	}
 
-
-  return(
-    <div>
-      <input onChange={(e) => setID(e.target.value)} value={ID} placeholder="ID" />
-      <input onChange={(e) => setPW(e.target.value)} value={PW} placeholder="PW" />  {/*이 때 로그인을 누르면 서버에서 ID값에 맞는 PW가 있으면
-      로그인 시킴*/ }
-      <button width={380} height={100} onClick={gomain}>Login</button>
-    </div>
-  )
+	return (
+		<div
+			style={{
+				display: "flex",
+				flexDirection: "column",
+				alignItems: "center", // 가로로 가운데 정렬
+				justifyContent: "center", // 세로로 중앙에 정렬
+				minHeight: "100vh", // 최소한 화면 높이만큼의 높이 설정
+				padding: "40px",
+				gap: "20px",
+				position: "fixed", // 화면에 고정
+				top: 0, // 상단에 정렬 (bottom 대신 top 사용)
+				left: 0, // 왼쪽에 정렬
+				right: 0, // 오른쪽에 정렬
+				bottom: 0, // 하단에 정렬
+				backgroundColor: "#CBEDD5", // 배경색 지정
+			}}
+		>
+			<input
+				onChange={(e) => setID(e.target.value)}
+				value={ID}
+				placeholder="ID"
+				style={{
+					width: "100%",
+					height: "40px",
+					backgroundColor: "#fff",
+					border: "none",
+				}}
+			/>
+			<input
+				onChange={(e) => setPW(e.target.value)}
+				value={PW}
+				placeholder="PW"
+				style={{
+					width: "100%",
+					height: "40px",
+					backgroundColor: "#fff",
+					border: "none",
+					marginBottom: "20px",
+				}}
+			/>
+			<button
+				width={380}
+				height={100}
+				onClick={gomain}
+				style={{
+					width: "100%",
+					height: "40px",
+					backgroundColor: "#fff",
+					border: "none",
+					boxShadow: "2px 2px 2px 2px lightGray",
+				}}
+			>
+				Login
+			</button>
+		</div>
+	);
 };
 
 export default Login;
